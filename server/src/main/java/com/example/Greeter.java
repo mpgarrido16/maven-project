fd15271d657b9887b6dfdffff978c454fa018dec
manuel <mpgarrido@gmail.com>
2022-09-11 17:35:10 -0500

    Pruebas de build 3

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hi world, %s!", someone);
+    return String.format("HELLOW WORLD, %s!", someone);
   }
 }
